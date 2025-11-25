0afbe698d8a35f6d1d9578822c2b71038e1f72fb
Álvaro Martín Reyes <amarrey2001@g.educaand.es>
2025-11-25 17:00:22 +0100

    Agrego la rutas para mostrar los pedidos

---
+++ routes/pedidoRouter.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+const pedidoController = require('../controllers/pedidoController')
+
+router.get('/', pedidoController.pedidos)
+
+router.get('/edit/:id', pedidoController.pedidoUpdateForm)
+router.post('/edit/:id', pedidoController.pedidoUpdate)
+
+router.get
